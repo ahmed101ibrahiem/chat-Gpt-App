deb9b61cdd817b66cdf2e5c2a635a0017e3e823e
Ahmed Ibrahim Elsabaa <ahmed101ibrahiem>
2023-04-17 22:22:08 +0200

    update api key

--- lib/constants/api_constant.dart
+++ lib/constants/api_constant.dart
@@ -4,7 +4,7 @@ class ApiConstant{
  static const String baseUrl
   = "https://api.openai.com/v1";
  static const String apiKey
-  = 'Bearer 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';
+  = 'Bearer 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';
  static const String contentType = 'Content-Type';
  static const String applicationJson = 'application/json';
 }
